bb8cd948b155eeb25cba9fbdfb31db0e2ee5d1ea
Alex <al12gamer@users.noreply.github.com>
2022-10-01 16:49:32 -0600

    Update deckscript.sh
    updated wording

--- deckscript.sh
+++ deckscript.sh
@@ -20,7 +20,7 @@ echo '
 Include = /etc/pacman.d/mirrorlist' | sudo tee --append /etc/pacman.conf
 sudo pacman --noconfirm -Syyu git go base-devel lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader neofetch vim 
 flatpak install brave
-## oh yeah let's now compile btop++ if we can
+## oh yeah let's now add btop++ if we can
 wget -qO btop.tbz https://github.com/aristocratos/btop/releases/latest/download/btop-x86_64-linux-musl.tbz
 sudo tar xf btop.tbz -C /usr/local bin/btop
 ## should be done at this point, but might need furher tweaking
