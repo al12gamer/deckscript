32afbbfc5c60036b3f4048cfe3c57d21f9061a37
Alex <al12gamer@users.noreply.github.com>
2022-10-02 01:18:27 -0600

    Update deckscript.sh
    packages

--- deckscript.sh
+++ deckscript.sh
@@ -20,8 +20,8 @@ sudo pacman -Sy archlinux-keyring && pacman -Su
 echo '
 [multilib]
 Include = /etc/pacman.d/mirrorlist' | sudo tee --append /etc/pacman.conf
-sudo pacman --noconfirm -Syyu git go base-devel lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader neofetch vim 
-flatpak install brave
+sudo pacman --noconfirm -Syyu git go base-devel lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader neofetch vim cmake
+flatpak install brave firefox
 ## oh yeah let's now add btop++ if we can
 wget -qO btop.tbz https://github.com/aristocratos/btop/releases/latest/download/btop-x86_64-linux-musl.tbz
 sudo tar xf btop.tbz -C /usr/local bin/btop
