bae3a8263e533bcecd009590ac6ea8447e793087
Alex <al12gamer@users.noreply.github.com>
2022-10-02 18:19:33 -0600

    Update deckscript.sh
    add discord

--- deckscript.sh
+++ deckscript.sh
@@ -17,7 +17,7 @@ sudo steamos-readonly disable
 flatpak update --appstream
 flatpak update -y
 flatpak install codium boxes lutris pupgui2
-flatpak install brave firefox
+flatpak install brave firefox discord
 ## pupgui2 is protonup-qt, also we may need to initialize pacman keys here as that helped on my deck
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
