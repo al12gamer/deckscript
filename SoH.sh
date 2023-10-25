2b6f0dd0fbc252d2701a07e4987f0feb361e0bb0
Alex <al12gamer@users.noreply.github.com>
2023-10-25 11:15:41 -0600

    Update SoH.sh
    updated opening of github page instead of heading to discord- looks like development is now done on github instead

--- SoH.sh
+++ SoH.sh
@@ -11,4 +11,4 @@ echo "Finished, your files are in the SoH folder."
 sleep 2
 echo "...if you would like to download Ship of Harkinian, we will now open up their web page so you can join their discord"
 sleep 4
-firefox https://discord.com/invite/shipofharkinian
+firefox https://github.com/HarbourMasters/Shipwright/releases/
