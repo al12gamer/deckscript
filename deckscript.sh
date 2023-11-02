a4a0a0ea534e53b1cb79ba12faa8261909fe48c6
Alex <al12gamer@users.noreply.github.com>
2023-11-02 15:37:03 -0600

    Update deckscript.sh
    Add SyncThingy flatpak back in for syncing saves that don't have cloud options

--- deckscript.sh
+++ deckscript.sh
@@ -23,7 +23,7 @@ nix-env -iA nixpkgs.firefox nixpkgs.brave nixpkgs.vscodium nixpkgs.gnome.gnome-b
 cd
 flatpak update --appstream
 flatpak update -y
-flatpak install -y pupgui2 yuzu dolphin
+flatpak install -y pupgui2 yuzu dolphin com.github.zocker_160.SyncThingy
 ## pull most recent Retroarch cores, if you have retroarch installed
 cd
 cd Desktop
