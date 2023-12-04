3a000021afb299bc8a0bc3a029832968ef1c7937
Alex <al12gamer@users.noreply.github.com>
2023-12-04 14:27:46 -0700

    Update deckscript.sh
    moving firefox from nix package to flatpak, since we don't know if something like #17 will happen again

--- deckscript.sh
+++ deckscript.sh
@@ -21,11 +21,11 @@ echo ********************************
 sleep 4
 sudo chown deck:deck /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
-nix-env -iA nixpkgs.firefox nixpkgs.brave nixpkgs.vscodium nixpkgs.gnome.gnome-boxes nixpkgs.btop nixpkgs.jotta-cli nixpkgs.fastfetch
+nix-env -iA nixpkgs.brave nixpkgs.vscodium nixpkgs.gnome.gnome-boxes nixpkgs.btop nixpkgs.jotta-cli nixpkgs.fastfetch
 cd
 flatpak update --appstream
 flatpak update -y
-flatpak install -y pupgui2 yuzu dolphin com.github.zocker_160.SyncThingy net.veloren.airshipper
+flatpak install -y pupgui2 yuzu dolphin com.github.zocker_160.SyncThingy net.veloren.airshipper org.mozilla.firefox  
 ## pull most recent Retroarch cores, if you have retroarch installed
 cd
 cd Desktop
