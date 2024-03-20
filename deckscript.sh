cde021a96604dac4df23ede9c52abf43a45db520
Alex <al12gamer@users.noreply.github.com>
2024-03-20 13:30:10 -0600

    Update deckscript.sh
    remove syncthingy and gnome boxes as not entirely necessary

--- deckscript.sh
+++ deckscript.sh
@@ -18,11 +18,11 @@ sleep 4
 sudo chown deck:deck /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 cd
-nix-env --install nixpkgs.brave nixpkgs.vscodium nixpkgs.gnome.gnome-boxes nixpkgs.btop nixpkgs.fastfetch
+nix-env --install nixpkgs.brave nixpkgs.vscodium nixpkgs.btop nixpkgs.fastfetch
 cd
 flatpak update --appstream
 flatpak update -y
-flatpak install -y pupgui2 dolphin com.github.zocker_160.SyncThingy net.veloren.airshipper org.mozilla.firefox lutris
+flatpak install -y pupgui2 dolphin net.veloren.airshipper org.mozilla.firefox lutris
 ## pull most recent Retroarch cores, if you have retroarch installed
 cd
 cd Desktop
